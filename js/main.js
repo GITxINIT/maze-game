d75162c78bf2d5390e6bfded97e0f2c2433f085e
Konstantin Simeonov <konstantin.simeonov@chaosgroup.com>
2019-01-19 18:27:43 +0200

    Fix some stuff
    This reverts commit 2bc8ea680805cbe2e7885917be82c0aad7fdf9f7.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (() => {
-	const editor = ace.edit(`editor`);
-	editor.setTheme(`ace/theme/monokai`);
+	const editor = ace.edit('editor');
+	editor.setTheme('ace/theme/monokai');
 	editor.session.setMode("ace/mode/javascript");
 	editor.setValue(`/*
 
@@ -27,14 +27,14 @@ const step = (up, left, right, down, from) => {
 	`;
 
 	const picassos = ((root) => mazes.map(maze => {
-		const maze_container = document.createElement(`div`);
-		maze_container.className = `maze-container`;
+		const maze_container = document.createElement('div');
+		maze_container.className = 'maze-container';
 		root.appendChild(maze_container);
 
 		const picasso = init_picasso(maze_container, maze);
 		picasso.draw();
 		return picasso;
-	}))(document.getElementById(`maze`));
+	}))(document.getElementById('maze'));
 
 	const _once = (name, fn, emitter) => {
 		const one_time_action = (...args) => {
@@ -45,17 +45,9 @@ const step = (up, left, right, down, from) => {
 		emitter.addEventListener(name, one_time_action);
 	};
 
-	// chrome won`t load the worker script via file:// protocol -_-
-	const _haxx_for_chrome = URL.createObjectURL(
-		new Blob(
-			[`(${String(init_worker)})()`],
-			{type: `text/javascript`}
-		)
-	);
-
 	const init_step_runner = async code => {
-		const worker = new Worker(_haxx_for_chrome);
-		worker.postMessage({name: `init`, code});
+		const worker = new Worker('js/worker.js');
+		worker.postMessage({ name: 'init', code });
 
 		const step = (...data) => new Promise((resolve, reject) => {
 			_once(`message`, message => resolve(message.data), worker);
@@ -70,7 +62,7 @@ const step = (up, left, right, down, from) => {
 	};
 
 	const start = (runners = []) => () => picassos.map(async (picasso, i) => {
-		const {clear, draw, fill, success, fail} = picasso;
+		const { clear, draw, fill, success, fail } = picasso;
 		clear(); draw();
 
 		const runner = await init_step_runner(get_solution());
@@ -79,7 +71,7 @@ const step = (up, left, right, down, from) => {
 		}
 
 		runners[i] = runner;
-		const {error, steps, terminated} = await play(
+		const { error, steps, terminated } = await play(
 			fill,
 			mazes[i],
 			runner
@@ -96,5 +88,5 @@ const step = (up, left, right, down, from) => {
 		return success(steps)
 	});
 
-	document.getElementById(`start-btn`).addEventListener(`click`, start());
+	document.getElementById('start-btn').addEventListener('click', start());
 })();
--- js/worker.js
+++ js/worker.js
@@ -1,18 +1,16 @@
-const init_worker = () => {
-	const handle_message = step => ({ data }) => {
-		const { name, code } = data;
-	
-		if (name === 'init') {
-			step = eval(code);
-			return;
-		}
-	
-		try {
-			self.postMessage({ direction: step(...data) });
-		} catch({ message, stack, name }) {
-			self.postMessage({ error: { message, stack, name } });
-		}
-	};
-	
-	self.onmessage = handle_message();
+const handle_message = step => ({ data }) => {
+	const { name, code } = data;
+
+	if (name === 'init') {
+		step = eval(code);
+		return;
+	}
+
+	try {
+		self.postMessage({ direction: step(...data) });
+	} catch({ message, stack, name }) {
+		self.postMessage({ error: { message, stack, name } });
+	}
 }
+
+self.onmessage = handle_message()
